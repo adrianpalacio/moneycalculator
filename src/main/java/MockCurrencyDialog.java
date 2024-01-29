5d238e070a22fe2921eacd391df0f4108711dc7e
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 13:40:29 +0000

    Update MockCurrencyDialog.java

--- src/main/java/MockCurrencyDialog.java
+++ src/main/java/MockCurrencyDialog.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class MockCurrencyDialog {
+package software.ulpgc.moneycalculator.mock;
+
+import software.ulpgc.moneycalculator.CurrencyDialog;
+import software.ulpgc.moneycalculator.model.Currency;
+
+import java.util.List;
+
+public class MockCurrencyDialog implements CurrencyDialog {
+    private List<Currency> currencies;
+
+    @Override
+    public CurrencyDialog define(List<Currency> currencies) {
+        this.currencies = currencies;
+        return this;
+    }
+
+    @Override
+    public Currency get() {
+        return currencies.get(1);
+    }
 }
