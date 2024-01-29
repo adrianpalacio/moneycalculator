ff48efbc55428ee5ecf00e432e80c7d580989ed5
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 13:53:40 +0000

    Update MoneyDialog.java

--- src/main/java/MoneyDialog.java
+++ src/main/java/MoneyDialog.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class MoneyDialog {
+package software.ulpgc.moneycalculator;
+
+import software.ulpgc.moneycalculator.model.Currency;
+import software.ulpgc.moneycalculator.model.Money;
+
+import java.util.List;
+
+public interface MoneyDialog {
+    MoneyDialog define(List<Currency> currencies);
+    Money get();
 }
