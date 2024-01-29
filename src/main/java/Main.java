a557e177893f1df6e6430008f7ab9647ea9602f0
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 13:52:04 +0000

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+package software.ulpgc.moneycalculator;
+
+import software.ulpgc.moneycalculator.cli.CliCurrencyDialog;
+import software.ulpgc.moneycalculator.cli.CliMoneyDialog;
+import software.ulpgc.moneycalculator.control.ExchangeCommand;
+import software.ulpgc.moneycalculator.mock.*;
+import software.ulpgc.moneycalculator.model.Currency;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MockCurrencyLoader currencyLoader = new MockCurrencyLoader();
+        List<Currency> currencies = currencyLoader.load();
+        MoneyDialog moneyDialog = new CliMoneyDialog().define(currencies);
+        CurrencyDialog currencyDialog = new CliCurrencyDialog().define(currencies);
+        MoneyDisplay moneyDisplay = new MockMoneyDisplay();
+        ExchangeRateLoader exchangeRateLoader = new MockExchangeRateLoader();
+        ExchangeCommand exchangeCommand = new ExchangeCommand(moneyDialog, currencyDialog, exchangeRateLoader, moneyDisplay);
+        exchangeCommand.execute();
+
+    }
 }
