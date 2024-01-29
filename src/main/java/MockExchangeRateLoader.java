9eef6b77f84a14a6e3ebabba40d8f31d2d76231a
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 13:41:23 +0000

    Update MockExchangeRateLoader.java

--- src/main/java/MockExchangeRateLoader.java
+++ src/main/java/MockExchangeRateLoader.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class MockExchangeRateLoader {
+package software.ulpgc.moneycalculator.mock;
+
+import software.ulpgc.moneycalculator.ExchangeRateLoader;
+import software.ulpgc.moneycalculator.model.Currency;
+import software.ulpgc.moneycalculator.model.ExchangeRate;
+
+import java.time.LocalDate;
+
+public class MockExchangeRateLoader implements ExchangeRateLoader {
+    @Override
+    public ExchangeRate load(Currency from, Currency to) {
+        return new ExchangeRate(from, to, LocalDate.now(), 1.2312);
+    }
 }
