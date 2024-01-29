5ac3622769c85b9754415a187e97674675b84400
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 13:37:10 +0000

    Update CliCurrencyDialog.java

--- src/main/java/CliCurrencyDialog.java
+++ src/main/java/CliCurrencyDialog.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class CliCurrencyDialog {
+package software.ulpgc.moneycalculator.cli;
+
+import software.ulpgc.moneycalculator.CurrencyDialog;
+import software.ulpgc.moneycalculator.model.Currency;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class CliCurrencyDialog implements CurrencyDialog {
+    private List<Currency> currencies;
+
+    @Override
+    public CurrencyDialog define(List<Currency> currencies) {
+        this.currencies = currencies;
+        return this;
+    }
+
+    @Override
+    public Currency get() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Introduzca una divisqa");
+            String code = scanner.next();
+            if (existCurrency(code)) return findCurrency(code);
+            System.out.println(code + " no encontrado");
+        }
+    }
+
+    private boolean existCurrency(String code) {
+        return findCurrency(code) != null;
+    }
+
+    private Currency findCurrency(String code) {
+        return currencies.stream()
+                .filter(c -> c.code().equalsIgnoreCase(code))
+                .findFirst()
+                .orElse(null);
+    }
 }
+
