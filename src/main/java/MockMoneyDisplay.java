65334a8e8b2a197e657ad3c97cebdf3db6631e21
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 13:43:00 +0000

    Update MockMoneyDisplay.java

--- src/main/java/MockMoneyDisplay.java
+++ src/main/java/MockMoneyDisplay.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class MockMoneyDisplay {
+package software.ulpgc.moneycalculator.mock;
+
+import software.ulpgc.moneycalculator.MoneyDisplay;
+import software.ulpgc.moneycalculator.model.Money;
+
+public class MockMoneyDisplay implements MoneyDisplay {
+    @Override
+    public void show(Money money) {
+        System.out.println(money);
+    }
 }
+
